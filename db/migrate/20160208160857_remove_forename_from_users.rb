8afe62dcb9878e133fafe7b68e7cd2d8e8a36818
myschoolpal <steve.howse@myschoolpal.co.uk>
2016-02-09 08:51:00 +0000

    forename

--- db/migrate/20160208160857_remove_forename_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForenameFromUsers < ActiveRecord::Migration
-  def change
-  	remove_column :users, :forename 
-  end
-end
---
+++ db/migrate/20160209084159_add_forename.rb
@@ -0,0 +1,5 @@
+class AddForename < ActiveRecord::Migration
+  def change
+  	add_column :users, :forename, :string
+  end
+end
